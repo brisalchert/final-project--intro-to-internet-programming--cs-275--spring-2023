949b711769bacbbbf69206f776b8462f432e39ca
Brian Salchert <brisalchert@gmail.com>
2023-04-25 10:38:02 -0400

    Added comment for input checking

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,6 +2,7 @@ window.onload = () => {
     // Add your code below
     let dimension = window.prompt(`Enter the size of your square matrix:`);
 
+    // Check that the input is an integer greater than 1
     while (isNaN(parseInt(dimension)) || parseInt(dimension) < 2) {
         alert(`Invalid input. Please try again.`);
 
