977cef9d64f76a00da5fa94fa36a1a0ef7a7f66d
brisalchert <brisalchert@gmail.com>
2023-04-25 13:32:21 -0400

    Added js for flipped matrix

--- dev/js/app.js
+++ dev/js/app.js
@@ -42,4 +42,39 @@ window.onload = () => {
         // Add the row to the matrix
         originalMatrix.appendChild(matrixRow);
     }
+
+    // Print the header for the flipped matrix
+    let flippedMatrixHeader = document.createElement(`h2`);
+    flippedMatrixHeader.innerText += `Flipped Matrix`;
+    matrixContainer.appendChild(flippedMatrixHeader);
+
+    // Print the flipped matrix
+    let flippedMatrix = document.createElement(`table`);
+    matrixContainer.appendChild(flippedMatrix);
+
+    for (let row = 0; row < dimension; row++) {
+        let matrixRow = document.createElement(`tr`);
+
+        for (let column = 0; column < dimension; column++) {
+            let matrixCell = document.createElement(`td`);
+
+            // Set the cell value to the flipped value from the original matrix,
+            // unless it is on the diagonal
+            if ((row + column) === (dimension - 1)) {
+                matrixCell.className = `diagonal`;
+                let cellValue = ((row * dimension) + column + 1);
+                matrixCell.innerText += `${cellValue}`;
+            }
+            else {
+                let cellValue = ((dimension * dimension) - ((row * dimension) + column));
+                matrixCell.innerText += `${cellValue}`;
+            }
+
+            // Add the cell to the matrix row
+            matrixRow.appendChild(matrixCell);
+        }
+
+        // Add the row to the flipped matrix
+        flippedMatrix.appendChild(matrixRow);
+    }
 };
